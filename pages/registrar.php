df6d678e8c82322de6b6621f026a9deb41c175af
danielcarlos3 <89228687+danielcarlos3@users.noreply.github.com>
2021-08-25 21:56:55 -0400

    pagina de cadastro criado

--- pages/registrar.php
+++ pages/registrar.php
@@ -1,40 +1,170 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="templete/AdminLTE-3.1.0/plugins/fontawesome-free/css/all.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="templete/AdminLTE-3.1.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="templete/AdminLTE-3.1.0/dist/css/adminlte.min.css">
-    <!-- estilo local -->
-    <link rel="stylesheet" href="./public/css/style.css">
-    <!-- meu icone -->
-    <link rel="icon" type="imagem/png" href="<?=URL_BASE;?>/public/imagens/cadeado.png" />
-    <title>Cadastro</title>
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="card card-primary">
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Endereço de email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-    <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Senha</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Enviar</button>
-</form>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <!-- estilo local -->
+        <link rel="stylesheet" href="../public/css/style.css">
+    <title>CADASTRO</title>
+  </head>
+<body class="pgcadastro">
+<!------ Include the above in your HEAD tag ---------->
+
+<form class="form-horizontal ">
+<fieldset>
+<div class="panel panel-primary ">
+    <div class="panel-heading pgcadastro">Cadastro de usuario</div>
+    
+    <div class="panel-body pgcadastro">
+<div class="form-group">
+<div class="col-md-11 control-label">
+        <p class="help-block"><h11>*</h11> Campo Obrigatório </p>
 </div>
-</body>
+</div>
+
+<!-- Text input nome-->
+<div class="form-group">
+  <label class="col-md-2 control-label" for="Nome">Nome <h11>*</h11></label>  
+  <div class="col-md-8">
+  <input id="Nome" name="Nome" placeholder="" class="form-control input-md" required="" type="text">
+  </div>
+</div>
+
+<!-- Text input email-->
+<div class="form-group">
+  <label class="col-md-2 control-label" for="prependedtext">Email <h11>*</h11></label>
+  <div class="col-md-5">
+    <div class="input-group">
+      <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="email@email.com" required="" type="text" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
+    </div>
+  </div>
+</div>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-2 control-label" for="Nome">SENHA <h11>*</h11></label>  
+  <div class="col-md-2">
+  <input id="senha" name="senha" placeholder="senha" class="form-control input-md" required="" type="password">
+  </div>
+
+  <label class="col-md-1 control-label" for="prependedtext">SENHA<h11>*</h11></label>
+     <div class="col-md-2">
+    <div class="input-group">
+    <input id="senha" name="senha" placeholder="senha" class="form-control input-md" required="" type="password">    
+  </div>
+  </div>
+  
+  
+  <label class="col-md-1 control-label" for="Nome">Nascimento<h11>*</h11></label>  
+  <div class="col-md-2">
+  <input id="dtnasc" name="dtnasc" placeholder="DD/MM/AAAA" class="form-control input-md" required="" type="date" maxlength="10" onkeypress="formatar('##/##/####', this)" onblur="showhide()">
+</div>
+
+<!-- Multiple Radios (inline) -->
+
+
+</div>
+
+<!-- Prepended text-->
+<div class="form-group">
+  <label class="col-md-2 control-label" for="Nome">CPF <h11>*</h11></label>  
+  <div class="col-md-2">
+  <input id="cpf" name="cpf" placeholder="Apenas números" class="form-control input-md" required="" type="text" maxlength="11" pattern="[0-9]+$">
+  </div>
+  
+    <label class="col-md-1 control-label" for="prependedtext">Telefone</label>
+     <div class="col-md-2">
+    <div class="input-group">
+      <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="XX XXXXX-XXXX" type="text" maxlength="13" pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$" onkeypress="formatar('## #####-####', this)">
+    </div>
+  </div>
+
+  <label class="col-md-1 control-label" for="radios">Sexo <h11>*</h11></label>
+  <div class="col-md-4"> 
+    <label required="" class="radio-inline" for="radios-0">
+      <input name="sexo" id="sexo" value="feminino" type="radio" required="">
+      Feminino
+    </label> 
+    <label class="radio-inline" for="radios-1">
+      <input name="sexo" id="sexo" value="masculino" type="radio">
+      Masculino
+    </label>
+  </div>
+ </div> 
+
+<!-- Prepended text-->
+<div class="form-group">
+  <label class="col-md-2 control-label" for="prependedtext">Endereço</label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <span class="input-group-addon">Rua</span>
+      <input id="rua" name="rua" class="form-control" placeholder="" required=""  type="text">
+    </div>
+    
+  </div>
+    <div class="col-md-2">
+    <div class="input-group">
+      <span class="input-group-addon">Nº <h11>*</h11></span>
+      <input id="numero" name="numero" class="form-control" placeholder="" required="" type="text">
+    </div>
+    
+  </div>
+  
+  <div class="col-md-3">
+    <div class="input-group">
+      <span class="input-group-addon">Bairro</span>
+      <input id="bairro" name="bairro" class="form-control" placeholder="" required=""  type="text">
+    </div>
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-2 control-label" for="prependedtext"></label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <span class="input-group-addon">Cidade</span>
+      <input id="cidade" name="cidade" class="form-control" placeholder="" required=""  type="text">
+    </div>
+    
+  </div>
+  
+   <div class="col-md-2">
+    <div class="input-group">
+      <span class="input-group-addon">Estado</span>
+      <input id="estado" name="estado" class="form-control" placeholder="" required=""  type="text">
+    </div>
+    
+  </div>
+</div>
+
+<!-- Button (Double) -->
+<div class="form-group">
+  <label class="col-md-2 control-label" for="Cadastrar"></label>
+  <div class="col-md-8">
+    <button id="Cadastrar" name="Cadastrar" class="btn btn-success" type="Submit">Cadastrar</button>
+    <button id="Cancelar" name="Cancelar" class="btn btn-danger" type="Reset">Cancelar</button>
+  </div>
+</div>
+
+</div>
+</div>
+
+
+</fieldset>
+</form>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  </body>
 </html>
